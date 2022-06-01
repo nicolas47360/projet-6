60a4008d5a29531bdec610fcf2dc611e0cd64bcf
nicolas47360 <nicem@hotmail.fr>
2022-06-01 15:17:02 +0200

    ajout likedislike controller et route post

--- backend/controllers/ctrlsauce.js
+++ backend/controllers/ctrlsauce.js
@@ -1,5 +1,6 @@
 const Sauce = require('../models/sauce');
 const fs = require('fs');
+const sauce = require('../models/sauce');
 
 exports.createSauce = (req, res, next) =>{
     const sauceObject = JSON.parse(req.body.sauce);
@@ -55,5 +56,47 @@ exports.getAllSauce = (req, res, next) => {
 }
 
 exports.likeDislike = (req, res, next) =>{
+    Sauce.findOne({_id: req.params.id})
+    .then((sauce) => {
+        if(req.body.like === 1 && !sauce.usersLiked.includes(req.body.userId)){
+            sauce.updateOne({_id: req.params.id},
+                {
+                    $inc: {likes: 1},
+                    $push: {usersLiked: req.body.userId}
+                })
+            .then(() => res.status(201).json({message:" sauce like"}))
+            .catch(error => res.status(400).json({error}))
+        }
 
+        else if(req.body.like === 0 && sauce.usersLiked.includes(req.body.userId)){
+            sauce.updateOne({_id: req.params.id},
+                {
+                    $inc: {likes: -1},
+                    $pull: {usersLiked: req.body.userId}
+                })
+            .then(() => res.status(201).json({message:" sauce dislike"}))
+            .catch(error => res.status(400).json({error}))
+        }
+
+        else if(req.body.like === -1 && !sauce.usersDisliked.includes(req.body.userId)){
+            sauce.updateOne({_id: req.params.id},
+                {
+                    $inc: {dislikes: 1},
+                    $push: {usersDisliked: req.body.userId}
+                })
+            .then(() => res.status(201).json({message:" sauce dislike"}))
+            .catch(error => res.status(400).json({error}))
+        }
+
+        else if(req.body.like === 0 && sauce.usersDisliked.includes(req.body.userId)){
+            sauce.updateOne({_id: req.params.id},
+                {
+                    $inc: {dislikes: -1},
+                    $pull: {usersDisliked: req.body.userId}
+                })
+            .then(() => res.status(201).json({message:" sauce dislike"}))
+            .catch(error => res.status(400).json({error}))
+        }
+    })
+    .catch(error => res.status(400).json({ error }));
 };
